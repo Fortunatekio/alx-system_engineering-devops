626bcf510ddc61bfbd59eceb199390b4147a8a2b
Fortunatekio <fortunatemugendi8@gmail.com>
2023-10-03 21:59:50 +0300

    repetition regular expressions

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,13 +1,2 @@
 #!/usr/bin/env ruby
-
-input_string = ARGV[0]
-
-pattern = /Holberton/i
-
-matches = input_string.scan(pattern)
-
-if matches.empty?
-  puts 'No match'
-else
-  puts matches.join(',')
-end
+#puts ARGV[0].match(/hbt{2,5}n/)
